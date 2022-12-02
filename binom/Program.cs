a5e4d1042ef8cbf1180f2b5155f1da8ba04e05c8
Видео <@->
2022-12-03 00:50:58 +0700

    Добавьте файлы проекта.

---
+++ binom/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace binom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                static int C(int n, int k)
+                {
+                    if (k == 0 || k == n)
+                        return 1;
+                    else return C(n - 1, k - 1) + C(n - 1, k);
+                }
+
+                int n = 0;
+                Console.WriteLine("Ведите степень: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int i = 0; i <= j; i++)
+                    {
+
+                        Console.Write(C(j, i) + ", ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
